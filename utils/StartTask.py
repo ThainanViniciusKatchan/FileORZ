5c1c534ea9634ec1c2b82dda2d734d76dce47554
Thainan <thaynanvinicyus1@gmail.com>
2026-06-09 16:37:09 -0400

    Melhoria na inicialização da organização, deixando ela de forma mais simples e funcional.

--- utils/StartTask.py
+++ utils/StartTask.py
@@ -1,51 +1,38 @@
+from tkinter import messagebox
 import os
 import ctypes
 import psutil
 import customtkinter
 from tkinter import messagebox
 from utils import StartUp
+from FileORZ import *
 import subprocess
 
+
 def check_if_running(taskname):
-    for proc in psutil.process_iter(['name']):
-        if proc.info['name'] == taskname:
+    for proc in psutil.process_iter(["name"]):
+        if proc.info["name"] == taskname:
             return True
     return False
 
+
 def start_task():
-    Startup = StartUp.StartUpSys().GetEnabled
+    try:
+        organize_files()
+    except Exception as Error:
+        print(f"Erro ao iniciar o Organizador: {Error}")
+        return False
+    return True
 
-    STATUS = check_if_running("FileORZ.exe")
-
-    SCRIPT_DIR = os.path.join(os.getcwd(), "dist", "FileORZ.exe")
-
-    if Startup:
-        if not STATUS:
-            if os.path.exists(SCRIPT_DIR):
-                try:
-                    subprocess.Popen([SCRIPT_DIR])
-                except Exception as Error:
-                    print(f"Erro ao iniciar o FileORZ.exe: {Error}")
-                return True
-            return None
-        else:
-            messagebox.showinfo("Erro", "FileORZ.exe ja esta em execução")
-            return False
-    else:
-        import FileORZ
-        try:
-            FileORZ.organize_files()
-        except Exception as Error:
-            print(f"Erro ao iniciar o FileORZ.py: {Error}")
-        return None
 
 def close_task():
     STATUS = check_if_running("FL_ORZ.exe")
     if STATUS:
-        subprocess.run(['taskkill', '/f', '/im', "FileORZ.exe"], check=True)
+        subprocess.run(["taskkill", "/f", "/im", "FileORZ.exe"], check=True)
+
 
 # Iniciar a organização
-def start_organizer(main_container, root, folder , feedback_label):
+def start_organizer(main_container, root, folder, feedback_label):
     # Remove label anterior se existir
     if feedback_label is not None:
         feedback_label.destroy()
@@ -56,10 +43,13 @@ def start_organizer(main_container, root, folder , feedback_label):
             main_container,
             text="Selecione uma pasta primeiro!",
             font=customtkinter.CTkFont(family="Segoe UI", size=13, weight="bold"),
-            text_color="red"
+            text_color="red",
         )
         feedback_label.pack(pady=(15, 0))
-        root.after(3000, lambda: feedback_label.destroy() if feedback_label.winfo_exists() else None)
+        root.after(
+            3000,
+            lambda: feedback_label.destroy() if feedback_label.winfo_exists() else None,
+        )
         return
     else:
         if start_task():
@@ -67,18 +57,28 @@ def start_organizer(main_container, root, folder , feedback_label):
                 main_container,
                 text="Organização concluída com sucesso!",
                 font=customtkinter.CTkFont(family="Segoe UI", size=13, weight="bold"),
-                text_color="green"
+                text_color="green",
             )
             feedback_label.pack(pady=(15, 0))
-            root.after(3000, lambda: feedback_label.destroy() if feedback_label.winfo_exists() else None)
+            root.after(
+                3000,
+                lambda: (
+                    feedback_label.destroy() if feedback_label.winfo_exists() else None
+                ),
+            )
         else:
             feedback_label = customtkinter.CTkLabel(
                 main_container,
                 text="Erro ao iniciar o organizador!",
                 font=customtkinter.CTkFont(family="Segoe UI", size=13, weight="bold"),
-                text_color="red"
+                text_color="red",
             )
             feedback_label.pack(pady=(15, 0))
-            root.after(3000, lambda: feedback_label.destroy() if feedback_label.winfo_exists() else None)
+            root.after(
+                3000,
+                lambda: (
+                    feedback_label.destroy() if feedback_label.winfo_exists() else None
+                ),
+            )
 
     # verifica se o processo do organizador já está funcionando
